2ad551306d933e79c2349f29b92378fe96612c03
kzeezlester <87939624+kzeezlester@users.noreply.github.com>
2023-11-27 22:23:50 +0100

    Add files via upload

---
+++ Destructuring-3.js
@@ -0,0 +1,10 @@
+/*Given a color array with three elements, 
+use array destructuring to create variables 
+firstColor, secondColor and < a i=3>which 
+contain the values ​​of the array.ThirdColor*/
+
+const colors = ["red", "green", "blue"];
+
+const [firstColor, secondColor, thirdColor] = colors;
+
+console.log(secondColor,firstColor,  thirdColor);
