8190a85a56ac31b249147161a5e2aa763154883a
kzeezlester <87939624+kzeezlester@users.noreply.github.com>
2023-11-28 02:14:26 +0100

    Add files via upload

---
+++ SpreadOperator-3.js
@@ -0,0 +1,8 @@
+/*Given an array originalArray, create a new array called cloneArray 
+that is a shallow copy of originalArray using the spread operator*/
+
+const originalArray = [1, 2, 3, 4, 5];
+let cloneArray = [...originalArray];
+console.log(cloneArray)
+
+
