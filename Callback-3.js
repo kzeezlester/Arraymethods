5b95dfaceb0bab84517f79e49d63fd77e8d40af8
kzeezlester <87939624+kzeezlester@users.noreply.github.com>
2023-12-04 00:31:50 +0100

    Add files via upload

---
+++ Callback-3.js
@@ -0,0 +1,26 @@
+/*Scrivi una funzione chiamata filterArray che accetta un array e una funzione di 
+callback come argomenti. -La funzione callback deve filtrare e restituire 
+i numeri pari passati come argomento alla funzione filterArray
+filterArray dovrebbe restituire un nuovo array contenente solo gli 
+elementi per i quali la funzione di callback restituisce true.*/
+
+
+function filterArray(arr, callback) {
+ let array= [];
+ for(let i = 0; i < arr.length; i++){
+   if(callback(arr[i])) {
+    array.push(arr[i]);
+   }
+ }
+ return array;
+}
+
+function isEven(num) {
+   return num % 2 === 0;
+}
+
+
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const evenNumbers = filterArray(numbers, isEven);
+console.log(evenNumbers); // Should output [2, 4, 6]
