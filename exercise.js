4a3302df0cc84adb7722cbd8834c719dffb91ea6
kzeezlester <87939624+kzeezlester@users.noreply.github.com>
2023-12-16 14:27:25 +0100

    Add files via upload

---
+++ exercise.js
@@ -0,0 +1,67 @@
+const persons = [
+    {
+      id: 1,
+      firstName: 'Mario',
+      lastName: 'Rossi',
+      age: 25
+    },
+    {
+      id: 2,
+      firstName: 'Maria',
+      lastName: 'Verdi',
+      age: 32
+    },
+    {
+      id: 3,
+      firstName: 'Giovanni',
+      lastName: 'Rossi',
+      age: 35
+    }
+  ];
+  
+  const jobs = [
+    {
+      id: 1,
+      jobTitle: 'CEO'
+    },
+    {
+      id: 2,
+      jobTitle: 'Project Manager'
+    },
+    {
+      id: 3,
+      jobTitle: 'Developer'
+    }
+  ];
+  
+  function fetchPersonById(id){
+    return promise1 = new Promise((resolve, reject) => {
+      setTimeout(() => {
+      const person = persons.find(person => person.id == id);
+      if (person){
+        return resolve(person)
+      } else {
+        return reject(`No person with id ${id}`)
+      }
+    },3000)
+    })
+  };
+  function fetchJobById(id){
+    return promise2 = new Promise((resolve, reject) => {
+      setTimeout(() => {
+      const job = jobs.find(job => job.id == id);
+      if (job){
+        return resolve(job)
+      } else {
+        return reject(`No job with id ${id}`)
+      }
+    },2000)
+    })
+  }
+
+  Promise.all([fetchPersonById(3), fetchJobById(1)])
+  .then(([person, job]) => {
+    console.log(`La persona trovata è ${person.firstName} e il suo lavoro è ${job.jobTitle}`);
+  })
+  .catch(error => console.error(error));
+ 
